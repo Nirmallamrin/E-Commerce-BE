c57d2fdadbaf5da9dee3f3c47a179b2167af7f06
[nirmal] <nirmallamrin77@gmail.com>
2024-06-21 09:15:02 +0530

    initial commit

--- index.js
+++ index.js
@@ -23,15 +23,15 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(
-  cors(
-    {
-      origin:"http://localhost:5173",
-      credentials:true,
-    }
-  ));
 
-// app.use(cors());
+
+const corsOptions = {
+  origin: ['http://localhost:5173', 'https://e-commerce-fe-cyan.vercel.app'], // Add your frontend URL here
+  optionsSuccessStatus: 200,
+  credentials:true,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(cookieParser());
