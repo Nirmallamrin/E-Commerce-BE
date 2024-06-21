b7edfa0ef5a44a1eb3c93746dfc70ea703d71c50
[nirmal] <nirmallamrin77@gmail.com>
2024-06-21 13:36:52 +0530

    initial commit

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -5,8 +5,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const razorpayInstance = new Razorpay ({
-    key_id: process.env.RAZORPAY_KEY_ID,
-    key_secret: process.env.RAZORPAY_KEY_SECRET
+    key_id: "rzp_test_L6wBhNL2VYpuYf",
+    key_secret:"3dcHujkEUhyBQBMTozHibuqK"
 });
 
 export const createOrder = async (req, res) => {
