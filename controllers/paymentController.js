41c3a06f982fb6e4e32714c2c68ce7576e1176d2
[nirmal] <nirmallamrin77@gmail.com>
2024-07-03 13:57:54 +0530

    initial commit

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const razorpayInstance = new Razorpay({
     key_id: "rzp_test_L6wBhNL2VYpuYf",
-    key_secret:"3dcHujkEUhyBQBMTozHibuqK"
+    key_secret: "3dcHujkEUhyBQBMTozHibuqK"
 });
 
 export const createOrder = async (req, res) => {
@@ -49,13 +49,13 @@ export const paymentVerify = async (req, res) => {
     try {
         const { razorpay_payment_id, razorpay_order_id, razorpay_signature } = req.body;
 
-        console.log('RAZORPAY_KEY_SECRET:', key_secret);  // Debugging line
+        console.log('RAZORPAY_KEY_SECRET:', process.env.RAZORPAY_KEY_SECRET);  // Debugging line
 
-        if (!key_secret) {
+        if (!process.env.RAZORPAY_KEY_SECRET) {
             return res.status(500).json({ message: 'Razorpay secret key not found' });
         }
 
-        const hmac = crypto.createHmac('sha256', key_secret);
+        const hmac = crypto.createHmac('sha256', process.env.RAZORPAY_KEY_SECRET);
 
         hmac.update(`${razorpay_order_id}|${razorpay_payment_id}`);
         const digest = hmac.digest('hex');
