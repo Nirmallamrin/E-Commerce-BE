d3759e72c80a6058632c74e214a8af01d6d45710
[nirmal] <nirmallamrin77@gmail.com>
2024-07-04 11:00:09 +0530

    initial commit

--- middlewares/upload-middleware.js
+++ middlewares/upload-middleware.js
@@ -1,12 +1,8 @@
 import multer from "multer";
-import path from 'path'
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/'); 
-  },
   filename: function (req, file, callback) {
-    callback(null, Date.now() + path.extname(file.originalname));
+    callback(null, file.originalname);
   },
 });
 
